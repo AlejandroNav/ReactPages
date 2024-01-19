dc1fcba6e81614150c6b9ffb116d6efc1988247e
Alex-Nav <alejugger@gmail.com>
2024-01-18 23:20:33 -0600

    changes

--- toDoReact/src/App.jsx
+++ toDoReact/src/App.jsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './app.css'
+import './styles.css'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [newItem, setNewItem] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='main-section'>
+      <form className='new-item-form' action="">
+        <div className="row">
+          <label htmlFor="item">Nueva Entrada</label>
+          <input type="text" id='item' />
+        </div>
+        <button className='button'>Anadir</button>
+      </form>
+      <h1 className='header'>Lista por hacer</h1>
+      <ul className='list'>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className='buttonDelete'>Borrar</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className='buttonDelete'>Borrar</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className='buttonDelete'>Borrar</button>
+        </li>
+      </ul>
+    </div>
   )
 }
 
