3963674e9c8fc9b39c3a4975f6531987dc9d6f6c
Alex-Nav <alejugger@gmail.com>
2024-01-21 23:09:31 -0600

    enter the text

--- toDoReact/src/App.jsx
+++ toDoReact/src/App.jsx
@@ -3,15 +3,26 @@ import './app.css'
 import './styles.css'
 function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos([...todos, { id: crypto.randomUUID(), title: newItem, completed: false }])
+  }
+
+  console.log(todos);
   return (
     <div className='main-section'>
-      <form className='new-item-form' action="">
+      <form onSubmit={handleSubmit} className='new-item-form' action="">
         <div className="row">
           <label htmlFor="item">Nueva Entrada</label>
           <input type="text" id='item' />
         </div>
         <button className='button'>Anadir</button>
       </form>
+
+
+      
       <h1 className='header'>Lista por hacer</h1>
       <ul className='list'>
         <li>
