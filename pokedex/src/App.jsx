558a590fb77c6482389183fb6021c6cb4795c55f
Alex-Nav <alejugger@gmail.com>
2024-01-22 23:09:05 -0600

    create new projects

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-2xl font-bold">Hola</h1>
+      <p className="text-2xl font-bold">
+        Pokedex
+      </p>
+    </>
+  )
+}
+
+export default App
