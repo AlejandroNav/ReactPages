b4ecd3f17e954ad0c6d6bcde853e40b03df18659
Alex-Nav <alejugger@gmail.com>
2024-01-24 02:09:34 -0600

    CHANGES

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,16 +1,14 @@
-import  Pokemons  from "./components/Pokemons/Pokemons";
-import  Aside  from "./components/Aside/Aside";
+import Pokemons from "./components/Pokemons/Pokemons";
+import Asides from "./components/Asides/Asides";
 
 function App() {
 
   return (
     <section>
-      <main>
-        <h1 className="text-2xl">Pokedex</h1>
+      <main className="font-outfit max-w-[1500px] mx-auto h-screen
+  bg-slate-300 grid grid-cols-1 lg:grid-cols-[1fr_350px]">
         <Pokemons />
-        <Aside />
-
-
+        <Asides />
       </main>
     </section>
   )
---
+++ pokedex/src/components/Asides/Asides.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Asides = () => {
+    return (
+        <div className='hidden lg:block '>
+            Asides
+        </div>
+    )
+}
+
+export default Asides
