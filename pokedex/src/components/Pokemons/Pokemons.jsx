d6cf4c008d4ed1e4939b80d1a1b356014ee8514e
Alex-Nav <alejugger@gmail.com>
2024-01-24 22:42:17 -0600

    testing api

--- pokedex/src/components/Pokemons/Pokemons.jsx
+++ pokedex/src/components/Pokemons/Pokemons.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import  axios  from 'axios'
+import React, { useEffect } from 'react'
 
 const Pokemons = () => {
-  //hola 
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=1024")
+      .then(({data})=> console.log(data))
+      .catch((err)=> console.log(err))
+  }, [])
+
+
+
   return (
-    <div>Charizard</div>
+    <section>Hola</section>
   )
 }
 
