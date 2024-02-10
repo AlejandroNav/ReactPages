c124d8bc75b2bc434985f9b87f8fe9d688e8d6f1
Alex-Nav <alejugger@gmail.com>
2024-02-10 10:57:19 -0600

    changes to ecvolution control

--- pokedex/src/components/Evolutions/Evolutions.jsx
+++ pokedex/src/components/Evolutions/Evolutions.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const Evolutions = ({ evolutions }) => {
     console.log(evolutions);
     return (
-        <div>
+        <div className='flex gap-4 items-center justify-center'>
             {evolutions.map((evolution, index) => (
-                <article key={index}>
-                    {evolution.name}
+                <article key={index} className='flex items-center capitalize '>
+                    <div >
+                        {index !== 0 && (<span>Lv. {evolution.min_level}</span>)
+                        }
+                        <h3>{evolution.name}</h3>
+                    </div>
+                    <img src={evolution.image} alt={evolution.name} />
+
                 </article>
             ))}
 
