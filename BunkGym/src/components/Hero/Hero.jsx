e22ea9cae3808aefee4f707576f444a47b53b2c7
Alex-Nav <alejugger@gmail.com>
2024-02-12 10:23:40 -0600

    basic scaffolding

---
+++ BunkGym/src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Hero/Hero.css'
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-left">
+                Izquierda
+            </div>
+            <div className="hero-right">
+                Derecha
+            </div>
+        </div>
+    )
+}
+
+export default Hero
