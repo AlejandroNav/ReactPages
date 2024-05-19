2b47a7d8dedf93a871218f1fe9f421cf99db200f
Alex-Nav <alejugger@gmail.com>
2024-05-19 16:03:56 -0600

    menu

--- BunkGym/src/components/Header/Header.jsx
+++ BunkGym/src/components/Header/Header.jsx
@@ -10,22 +10,37 @@ const Header = () => {
                 <img src={LogoWhite} alt="Bunkgym Logo" className="logo" />
             </Link>
             <ul className='header-menu'>
-                <Link to="/"  style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <li className='header-menu-item header-menu-item-text' >Home</li>
-                </Link>
-                <Link to="/quienesSomos"  style={{ textDecoration: 'none', color: 'inherit' }}>
+
+                <div className="dropdown">
+                    <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <li className='header-menu-item header-menu-item-text' >Home</li>
+                    </Link>
+                    <div className="dropdown-menu">
+                        <Link to="/personaliza" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <li className='header-menu-item header-menu-item-text'>Personaliza</li>
+                        </Link>
+                        <Link to="/planes" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <li className='header-menu-item header-menu-item-text'>Planes</li>
+                        </Link>
+                        <Link to="/contacto" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <li className='header-menu-item header-menu-item-text'>Contacto</li>
+                        </Link>
+                    </div>
+                </div>
+
+                <Link to="/quienesSomos" style={{ textDecoration: 'none', color: 'inherit' }}>
                     <li className='header-menu-item header-menu-item-text'>Nosotros</li>
                 </Link>
-                <Link to="/particulares"  style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link to="/particulares" style={{ textDecoration: 'none', color: 'inherit' }}>
                     <li className='header-menu-item  header-menu-item-text'>Clientes</li>
                 </Link>
-                <Link to="/crossfit"  style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link to="/crossfit" style={{ textDecoration: 'none', color: 'inherit' }}>
                     <li className='header-menu-item  header-menu-item-text'>BunkGym</li>
                 </Link>
-                <Link to="/gimnasios"  style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link to="/gimnasios" style={{ textDecoration: 'none', color: 'inherit' }}>
                     <li className='header-menu-item  header-menu-item-text'>+ Productos  </li>
                 </Link>
-                <Link to="/comunidades"  style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link to="/comunidades" style={{ textDecoration: 'none', color: 'inherit' }}>
                     <li className='header-menu-item header-menu-item-text'>Contacto</li>
                 </Link>
             </ul>
