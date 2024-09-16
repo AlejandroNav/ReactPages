82d8ebbb4f9110154a8cf1663ed59c64bb688231
Alex-Nav <alejugger@gmail.com>
2024-09-15 20:49:07 -0600

    last changes to powrlifter iamges

--- BunkGym/src/components/NuestroEquipo/NuestroEquipo.jsx
+++ BunkGym/src/components/NuestroEquipo/NuestroEquipo.jsx
@@ -11,6 +11,12 @@ import imagenDavid from '../../assets/fotopersona.png';
 import exampleimage from '../../assets/27.png';
 import adorno from '../../assets/adorno1.avif';
 
+
+import logolift from '../../assets/logolift.png';
+import logolift2 from '../../assets/logolift2.png';
+import personalift from '../../assets/persona.png';
+import personalift2 from '../../assets/persona2.png';
+
 const NuestroEquipo = () => {
     // Definir los miembros del equipo
     const miembros = [
@@ -52,6 +58,44 @@ const NuestroEquipo = () => {
                 <h2 className='nuestro-equipo-footer-frase'>HEMOS NACIDO POR Y PARA MEJORAR E INNOVAR EN EL
                     MUNDO DEL FITNESS
                 </h2>
+            </div>
+            <div className='nuestros-personas'>
+                <h2 className='stroke-text-black-title titulo-general'>Diseño, asesoramiento y apoyo de campeones para campeones </h2>
+
+                <div className='personas-fila'>
+                    <div className='persona-imagen-container'>
+                        <img src={personalift} alt="carlos" className='persona-imagen-img' />
+                    </div>
+                    <div className='persona-text-container'>
+                        <ul className='persona-ul'>
+
+                            <li>  ●   Campeón España Powerlifting AEP (4 Veces)</li>
+                            <li>  ●   Campeón España Press de Banco AEP (2 Veces)</li>
+                            <li>  ●   Campeón España Culturismo IFBB</li>
+                            <li>  ●   Campeón del Mundo Culturismo IFBB</li>
+                            <li>  ●   Carnet Profesional de la IFBB (IFBBPro)</li>
+
+                        </ul>
+                    </div>
+                    <div className='persona-imagen-container'>
+                        <img src={logolift2} alt="carlos" className='persona-imagen-img' />
+                    </div>
+                </div>
+                <div className='personas-fila'>
+                    <div className='persona-imagen-container'>
+                        <img src={logolift} alt="carlos" className='persona-imagen-img' />
+                    </div>
+                    <div className='persona-text-container'>
+                        <ul className='persona-ul'>
+                            <li>  ●  Campeón del Mundo Powerlifting WPC (Subjunior -100Kg) </li>
+                            <li>  ●  Récord de España Powerlifting de total (Subjunior -100Kg) </li>
+                        </ul>
+                    </div>
+                    <div className='persona-imagen-container'>
+                        <img src={personalift2} alt="carlos" className='persona-imagen-img' />
+                    </div>
+                </div>
+
             </div>
         </div>
     );
